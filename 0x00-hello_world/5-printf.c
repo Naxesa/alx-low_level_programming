43852f7472ff7d5329f8ad400c2676f4aad69d28
Naxesa <naxesa.foundation@yahoo.com>
2025-04-28 00:18:44 +0300

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0(Success)
+ * Printf: Prints the string
+ */
+int main(void)
+{
+	printfprintf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
