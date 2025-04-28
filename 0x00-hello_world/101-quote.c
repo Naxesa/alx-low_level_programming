0b41fef8f042c2dc238623c4b1cac70370b62e1f
Naxesa <naxesa.foundation@yahoo.com>
2025-04-29 00:40:12 +0300

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
- * Return: Always 1(per req)
- * Fprintf: Acts as print/Puts
+ * Return: Show an error(per req)
+ * write: Acts as print/Puts
  */
 int main(void)
 {
-	fprintf(stderr,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
