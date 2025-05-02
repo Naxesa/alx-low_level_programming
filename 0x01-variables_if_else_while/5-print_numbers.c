e08df53d1eb7066c8e0ebb9360e110793955eea7
Naxesa <naxesa.foundation@yahoo.com>
2025-05-02 23:45:59 +0300

    Q6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0 (Success)
+ */
+int main(void)
+{
+	char a = '0';
+
+
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+
+
+	putchar('\n');
+
+
+	return (0);
+}
