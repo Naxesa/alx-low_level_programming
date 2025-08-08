a85e15156beff1b0717a5b75d0aa8c89bb842c81
Naxesa <nexesa.foundation@yahoo.com>
2025-08-08 15:22:36 +0300

    tester and header files

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last dgt of a number
+ * @n: the int to extract the last from
+ * Return: value of the last dgt
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+		n = -n;
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+	_putchar(a + '0');
+
+	return (a);
+}
