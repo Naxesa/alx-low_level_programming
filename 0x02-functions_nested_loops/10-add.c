a6de49c73520abc01ad72035ed0a62ac15fc1acc
Naxesa <nexesa.foundation@yahoo.com>
2025-08-10 23:21:29 +0300

    10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two ints to get rslt
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: result of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
