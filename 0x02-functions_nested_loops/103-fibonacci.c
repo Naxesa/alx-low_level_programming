26b01e60b9e006b9b0268305b1f88078b82f5a47
Naxesa <nexesa.foundation@yahoo.com>
2025-08-13 09:56:56 +0000

    feat: add fibonacci function

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	unsigned long fib1 = 1, fib2 = 2, fib_next;
+
+	printf("%lu, %lu", fib1, fib2);
+
+	for (i = 2; i < 98; i++)
+	{
+		fib_next = fib1 + fib2;
+		printf(", %lu", fib_next);
+		fib1 = fib2;
+		fib2 = fib_next;
+	}
+
+	printf("\n");
+	return (0);
+}
