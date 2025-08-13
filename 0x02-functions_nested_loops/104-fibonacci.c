37ef620575af4f26b865e41d1ea4d2341439246f
Naxesa <nexesa.foundation@yahoo.com>
2025-08-13 10:22:04 +0000

    feat: add fibonacci function

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 98 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long a = 1, b = 2;
+	unsigned long a_high = 0, b_high = 0;
+	unsigned long sum, sum_high;
+	int count;
+
+	for (count = 1; count <= 98; count++)
+	{
+		if (a_high)
+		{
+			printf("%lu%010lu", a_high, a);
+		}
+		else
+		{
+			printf("%lu", a);
+		}
+
+		if (count != 98)
+		{
+			printf(", ");
+		}
+
+		sum = a + b;
+		sum_high = a_high + b_high;
+
+		if (sum < a)
+		{
+			sum_high += 1;
+		}
+
+		a = b;
+		a_high = b_high;
+		b = sum;
+		b_high = sum_high;
+	}
+	printf("\n");
+	return (0);
+}
